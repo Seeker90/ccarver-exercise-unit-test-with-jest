6d3ee8a140dc776b59f0d7d01745ebd78870d0bd
Seeker90 <chris.carver90@icloud.com>
2025-09-04 01:15:38 +0000

    final

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 test("One euro should be 1.07 dollars", function() {
-    // Import the function from app.js
     const { fromEuroToDollar } = require('./app.js');
     const dollars = fromEuroToDollar(3.5);
     const expected = 3.5 * 1.07; 
